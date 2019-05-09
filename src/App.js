ef5f03fc53eed5d341f6d47b8e445d8de256a10a
RasmusSild <rasmus.sild94@gmail.com>
2019-05-10 00:23:19 +0300

    bulma

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
 import SampleChart from './SampleChart';
+import SampleChart3 from './SampleChart3';
+import { Columns, Card } from "react-bulma-components/full";
 
 const App = () => (
   <div className="app">
-    <SampleChart />
+    <Columns>
+      <Columns.Column size={3}>
+        <Card>
+          <Card.Content>
+            <SampleChart3/>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Card.Content>
+            <SampleChart3/>
+          </Card.Content>
+        </Card>
+      </Columns.Column>
+      <Columns.Column size={9}>
+        <SampleChart />
+      </Columns.Column>
+    </Columns>
   </div>
 );
 
