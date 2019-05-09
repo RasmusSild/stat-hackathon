8c446c87b8f5b469828852cdc189d682a3ff8715
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-05-09 23:04:30 +0300

    Add types

--- src/SampleChart.js
+++ src/SampleChart.js
@@ -5,16 +5,17 @@ const SampleChart = () => {
 
     const chartRef = React.useRef(null);
     let coefficient = 1.005
+    let coefficient2 = 1.005
     const [inputValue, setInputValue] = React.useState(coefficient);
-    const [inputValue2, setInputValue2] = React.useState(coefficient);
+    const [inputValue2, setInputValue2] = React.useState(coefficient2);
     let i = 0;
-    let years = ['product', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'];
-    let _18_24data = ['18-24', 37.1, 32.1, 33.4, 32.6, 28.1, 30.1, 28.0, 30.1, 31.1, 33.8, 35.5, 28.0, 25.0, 30.6, 31.5, 31.8, 32.6, 35.7, 36.7, 39.7, 41.2];
-    let _25_49data = ['25-49', 76.8, 76.8, 75.0, 75.9, 76.5, 78.3, 78.1, 79.4, 84.4, 84.7, 84.3, 76.6, 74.8, 78.4, 79.7, 80.5, 80.9, 82.8, 82.1, 83.8, 83.9];
-    let _50_74data = ['50-74', 40.4, 39.8, 38.9, 39.7, 42.2, 42.8, 44.6, 46.1, 49.9, 50.8, 51.1, 48.5, 45.8, 49.0, 51.1, 52.8, 53.6, 54.9, 56.2, 57.3, 57.9];
-    let _18_24data2 = ['18-24(2)', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5.7, 4.4, 2.5, 3.3, 3.5, 3.3, 2.4, 1.8, 2.8, 1.8];
-    let _25_49data2 = ['25-49(2)', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 54.8, 54.5, 51.7, 48.5, 46.7, 48.5, 48.9, 47.7, 46.9, 47.3, 47.4];
-    let _50_74data2 = ['50-74(2)', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39.5, 41.1, 45.8, 48.2, 49.8, 48.2, 48.7, 50.4, 49.5, 49.9, 50.8];
+    let years = ['product', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'];
+    let _18_24data = ['18-24', 35.5, 28.0, 25.0, 30.6, 31.5, 31.8, 32.6, 35.7, 36.7, 39.7, 41.2];
+    let _25_49data = ['25-49', 84.3, 76.6, 74.8, 78.4, 79.7, 80.5, 80.9, 82.8, 82.1, 83.8, 83.9];
+    let _50_74data = ['50-74', 51.1, 48.5, 45.8, 49.0, 51.1, 52.8, 53.6, 54.9, 56.2, 57.3, 57.9];
+    let _18_24data2 = ['18-24(2)', 5.7, 4.4, 2.5, 3.3, 3.5, 3.3, 2.4, 1.8, 1.8, 2.8, 1.8];
+    let _25_49data2 = ['25-49(2)', 54.8, 54.5, 51.7, 48.5, 46.7, 48.5, 48.9, 47.7, 46.9, 47.3, 47.4];
+    let _50_74data2 = ['50-74(2)', 39.5, 41.1, 45.8, 48.2, 49.8, 48.2, 48.7, 50.4, 49.5, 49.9, 50.8];
 
     function calcNumber(number, inputValue) {
         let numberVal = (Number)(number[number.length - 1] * inputValue).toFixed(1);
@@ -63,27 +64,59 @@ const SampleChart = () => {
                 type: 'pie',
                 id: 'pie',
                 radius: '30%',
-                center: ['50%', '25%'],
+                center: ['35%', '25%'],
                 label: {
-                    formatter: '{b}: {@1998} ({d}%)'
+                    formatter: '{b}: {@2008} ({d}%)'
                 },
                 encode: {
                     itemName: 'product',
-                    value: '1998',
-                    tooltip: '1998'
-                }
+                    value: '2008',
+                    tooltip: '2008'
+                },
+                data: [_18_24data, _25_49data, _50_74data]
+            },
+            {
+                type: 'pie',
+                id: 'pie2',
+                radius: '30%',
+                center: ['65%', '25%'],
+                label: {
+                    formatter: '{b}: {@2008} ({d}%)'
+                },
+                encode: {
+                    itemName: 'product',
+                    value: '2008',
+                    tooltip: '2008'
+                },
+                data: [_18_24data2, _25_49data2, _50_74data2]
             }
         ]
     }
 
     let onEvents = {
         'updateAxisPointer': (event) => {
-            var xAxisInfo = event.axesInfo[0];
+            let xAxisInfo = event.axesInfo[0];
             if (xAxisInfo) {
-                var dimension = xAxisInfo.value + 1;
+                let dimension = xAxisInfo.value + 1;
                 chartRef.current.getEchartsInstance().setOption({
                     series: {
                         id: 'pie',
+                        label: {
+                            formatter: '{b}: {@[' + dimension + ']} ({d}%)'
+                        },
+                        encode: {
+                            value: dimension,
+                            tooltip: dimension
+                        }
+                    }
+                });
+            }
+            let xAxisInfo2 = event.axesInfo[0];
+            if (xAxisInfo2) {
+                let dimension = xAxisInfo2.value + 1;
+                chartRef.current.getEchartsInstance().setOption({
+                    series: {
+                        id: 'pie2',
                         label: {
                             formatter: '{b}: {@[' + dimension + ']} ({d}%)'
                         },
@@ -103,7 +136,7 @@ const SampleChart = () => {
             <label for="coef">Eestlaste tööhõivemäära trend</label>
             <input step="0.005" type="number" value={inputValue} id="coef"
                    onChange={(evt) => setInputValue(evt.target.value)}/>
-            <label htmlFor="coef">Välistööjõu tööhõivemäära trend</label>
+            <label htmlFor="coef2">Välistööjõu tööhõivemäära trend</label>
             <input step="0.005" type="number" value={inputValue2} id="coef"
                    onChange={(evt) => setInputValue2(evt.target.value)}/>
         </div>
