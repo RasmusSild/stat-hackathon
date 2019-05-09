d54a812e0035b2f9f25bc6534941c6c8a1792599
RasmusSild <rasmus.sild94@gmail.com>
2019-05-09 21:07:05 +0300

    input

--- src/SampleChart.js
+++ src/SampleChart.js
@@ -4,13 +4,14 @@ import ReactEcharts from "echarts-for-react";
 const SampleChart = () => {
 
     let coefficient = 1.005
+  const [inputValue, setInputValue] = React.useState(coefficient);
     let i = 0;
     let years = ['product', 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018];
     let _18_24data = ['18-24', 37.1, 32.1, 33.4, 32.6, 28.1, 30.1, 28.0, 30.1, 31.1, 33.8, 35.5, 28.0, 25.0, 30.6, 31.5, 31.8, 32.6, 35.7, 36.7, 39.7, 41.2];
     let _25_49data = ['25-49', 76.8, 75.0, 75.9, 76.5, 78.3, 78.1, 79.4, 84.4, 84.7, 84.3, 76.6, 74.8, 78.4, 79.7, 80.5, 80.9, 82.8, 82.1, 83.8, 83.9];
     let _50_74data = ['50-74', 40.4, 39.8, 38.9, 39.7, 42.2, 42.8, 44.6, 46.1, 49.9, 50.8, 51.1, 48.5, 45.8, 49.0, 51.1, 52.8, 53.6, 54.9, 56.2, 57.3, 57.9];
     function calcNumber(number) {
-        let numberVal = (Number)(number[number.length - 1] * coefficient).toFixed(1);
+        let numberVal = (Number)(number[number.length - 1] * inputValue).toFixed(1);
         return numberVal > 100 ? 100 : numberVal;
 
     }
@@ -73,6 +74,8 @@ const SampleChart = () => {
     return (
         <div className="chart">
             <ReactEcharts option={opts} style={{height: "70vh"}}/>
+          <label for="coef">Pane mingi nr</label>
+            <input step="0.005" type="number" value={inputValue} id="coef" onChange={(evt) => setInputValue(evt.target.value)} />
         </div>
     )
 };
