6a98512ad67218a49066bbb23e5642ce43daeaa4
RasmusSild <rasmus.sild94@gmail.com>
2019-05-09 21:50:51 +0300

    class component

--- src/SampleChart.js
+++ src/SampleChart.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import ReactEcharts from "echarts-for-react";
 
-const SampleChart = () => {
+class SampleChart extends React.Component {
 
-  const chartRef = React.useRef(null);
-    let coefficient = 1.005
-  const [inputValue, setInputValue] = React.useState(coefficient);
+  constructor(props) {
+    super(props);
+    this.coefficient = 1.005;
+    this.chartRef = React.createRef();
+
+    this.state = {
+      inputValue: this.coefficient
+    }
+
     let i = 0;
     let years = ['product', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'];
     let _18_24data = ['18-24', 37.1, 32.1, 33.4, 32.6, 28.1, 30.1, 28.0, 30.1, 31.1, 33.8, 35.5, 28.0, 25.0, 30.6, 31.5, 31.8, 32.6, 35.7, 36.7, 39.7, 41.2];
     let _25_49data = ['25-49', 76.8, 75.0, 75.9, 76.5, 78.3, 78.1, 79.4, 84.4, 84.7, 84.3, 76.6, 74.8, 78.4, 79.7, 80.5, 80.9, 82.8, 82.1, 83.8, 83.9];
     let _50_74data = ['50-74', 40.4, 39.8, 38.9, 39.7, 42.2, 42.8, 44.6, 46.1, 49.9, 50.8, 51.1, 48.5, 45.8, 49.0, 51.1, 52.8, 53.6, 54.9, 56.2, 57.3, 57.9];
-    function calcNumber(number) {
-        let numberVal = (Number)(number[number.length - 1] * inputValue).toFixed(1);
-        return numberVal > 100 ? 100 : numberVal;
 
-    }
     while (i < 22) {
-        years.push((Number) (years[years.length - 1] + 1).toFixed(0))
-        _18_24data.push(calcNumber(_18_24data));
-        _25_49data.push(calcNumber(_25_49data));
-        _50_74data.push(calcNumber(_50_74data));
-        i++;
+      years.push((Number) (years[years.length - 1] + 1).toFixed(0))
+      _18_24data.push(this.calcNumber(this.state.inputValue,_18_24data));
+      _25_49data.push(this.calcNumber(this.state.inputValue,_25_49data));
+      _50_74data.push(this.calcNumber(this.state.inputValue,_50_74data));
+      i++;
 
     }
-    const opts = {
+
+    this.opts = {
       legend: {},
       tooltip: {
         trigger: 'axis',
@@ -60,36 +63,51 @@ const SampleChart = () => {
           }
         }
       ]
-    }
+    };
 
-  let onEvents = {
-    'updateAxisPointer': (event) => {
-      var xAxisInfo = event.axesInfo[0];
-      if (xAxisInfo) {
-        var dimension = xAxisInfo.value + 1;
-        chartRef.getEchartsInstance().setOption({
-          series: {
-            id: 'pie',
-            label: {
-              formatter: '{b}: {@[' + dimension + ']} ({d}%)'
-            },
-            encode: {
-              value: dimension,
-              tooltip: dimension
+    this.onEvents = {
+      'updateAxisPointer': (event) => {
+        console.log(this.chartRef);
+        var xAxisInfo = event.axesInfo[0];
+        if (xAxisInfo) {
+          var dimension = xAxisInfo.value + 1;
+          this.chartRef.current.getEchartsInstance().setOption({
+            series: {
+              id: 'pie',
+              label: {
+                formatter: '{b}: {@[' + dimension + ']} ({d}%)'
+              },
+              encode: {
+                value: dimension,
+                tooltip: dimension
+              }
             }
-          }
-        });
+          });
+        }
       }
-    }
-  };
+    };
 
+  }
+
+    calcNumber = (inputValue, number) => {
+        let numberVal = (Number)(number[number.length - 1] * inputValue).toFixed(1);
+        return numberVal > 100 ? 100 : numberVal;
+
+    };
+
+
+
+
+
+  render() {
     return (
-        <div className="chart">
-            <ReactEcharts ref={chartRef} option={opts} style={{height: "70vh"}} />
-          <label for="coef">Pane mingi nr</label>
-            <input step="0.005" type="number" value={inputValue} id="coef" onChange={(evt) => setInputValue(evt.target.value)} />
-        </div>
+      <div className="chart">
+        <ReactEcharts ref={(e) => { this.chartRef.current = e; }} option={this.opts} style={{height: "70vh"}} onEvents={this.onEvents}/>
+        <label for="coef">Pane mingi nr</label>
+        <input step="0.005" type="number" value={this.state.inputValue} id="coef" onChange={(evt) => this.setState({inputValue: evt.target.value})} />
+      </div>
     )
+  }
 };
 
 
