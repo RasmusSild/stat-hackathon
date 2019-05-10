48dee4c57a9113b23c58ff564df0cd3532fdc882
Erki Work <erki_work@Erkis-MacBook-Pro.local>
2019-05-10 13:51:21 +0300

    clickable thumbs

--- src/pages/FutureTeller.js
+++ src/pages/FutureTeller.js
@@ -11,38 +11,49 @@ import Columns from 'react-bulma-components/lib/components/columns';
 import Card from 'react-bulma-components/lib/components/card';
 import Heading from 'react-bulma-components/lib/components/heading';
 
-const FutureTeller = () => (
-  <Container>
-    <Columns className="is-fullheight">
-      <Columns.Column size={12}>
-        <Heading size={3} renderAs="h2" >Foo</Heading>
-      </Columns.Column>
-      <Columns.Column size={3}>
-        <Card>
-          <Card.Content>
-            <ForeignDomainsThumbnail />
-          </Card.Content>
-        </Card>
-        <Card>
-          <Card.Content>
-            <NationalDomainsThumbnail />
-          </Card.Content>
-        </Card>
-      </Columns.Column>
-      <Columns.Column size={9}>
-        <Employment />
-      </Columns.Column>
-      <Columns.Column size={6}>
-        <NationalDomains />
-      </Columns.Column>
-      <Columns.Column size={6}>
-        <ForeignDomains />
-      </Columns.Column>
-      <Columns.Column size={12}>
-        <SampleChart2 />
-      </Columns.Column>
-    </Columns>
-  </Container>
-);
+const FutureTeller = () => {
+
+  const [activeChart, setActiveChart] = React.useState('employment');
+
+  return (
+    <Container>
+      <Columns className="is-fullheight">
+        <Columns.Column size={12}>
+          <Heading size={3} renderAs="h2" >Foo</Heading>
+        </Columns.Column>
+        <Columns.Column size={3}>
+          <Card onClick={() => setActiveChart('fDomains')}>
+            <Card.Content>
+              <ForeignDomainsThumbnail />
+            </Card.Content>
+          </Card>
+          <Card onClick={() => setActiveChart('nDomains')}>
+            <Card.Content>
+              <NationalDomainsThumbnail />
+            </Card.Content>
+          </Card>
+        </Columns.Column>
+        <Columns.Column size={9}>
+          {activeChart === 'employment' && <Employment />}
+          {activeChart === 'nDomains' && <NationalDomains />}
+          {activeChart === 'fDomains' && <ForeignDomains />}
+        </Columns.Column>
+        {/*
+          <>
+            <Columns.Column size={6}>
+              <NationalDomains />
+            </Columns.Column>
+            <Columns.Column size={6}>
+              <ForeignDomains />
+            </Columns.Column>
+            <Columns.Column size={12}>
+              <SampleChart2 />
+            </Columns.Column>
+          </>*/
+        }
+      </Columns>
+    </Container>
+  );
+};
 
 export default FutureTeller;
