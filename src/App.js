d644ef906997d107736d2cb8d24e48c6689b8965
mare hunt <mare.hunt@netgroup.ee>
2019-05-10 11:09:27 +0300

    added sass components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import SampleChart2Nat from './SampleChart2Nat';
 import SampleChart2Imm from './SampleChart2Imm';
 import SampleChart2 from './SampleChart2';
 import SampleChart3 from './SampleChart3';
-import {Container, Columns, Card} from "react-bulma-components/full";
+
+import Container from 'react-bulma-components/lib/components/container';
+import Columns from 'react-bulma-components/lib/components/columns';
+import Card from 'react-bulma-components/lib/components/card';
+import Heading from 'react-bulma-components/lib/components/heading';
 
 const App = () => (
     <Container>
         <Columns>
-        <h1>Foo</h1>
+            <Columns.Column size={12}>
+                <Heading size={3} renderAs="h2" >Foo</Heading>
+            </Columns.Column>
             <Columns.Column size={3}>
                 <Card>
                     <Card.Content>
