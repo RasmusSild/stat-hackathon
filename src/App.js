1770a56ad64aaf0775733ec3cfd1ef0e733b3042
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-05-10 09:58:56 +0300

    Revert "Updates"
    This reverts commit d2e926ac

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import SampleChart from './SampleChart';
+import SampleChart2Nat from './SampleChart2Nat';
+import SampleChart2Imm from './SampleChart2Imm';
+import SampleChart2 from './SampleChart2';
+import SampleChart3 from './SampleChart3';
+import {Columns, Card} from "react-bulma-components/full";
+
+const App = () => (
+    <div className="app">
+        <Columns>
+            <Columns.Column size={3}>
+                <Card>
+                    <Card.Content>
+                        <SampleChart3/>
+                    </Card.Content>
+                </Card>
+                <Card>
+                    <Card.Content>
+                        <SampleChart3/>
+                    </Card.Content>
+                </Card>
+            </Columns.Column>
+            <Columns.Column size={9}>
+                <SampleChart/>
+            </Columns.Column>
+            <Columns.Column size={6}>
+                <SampleChart2Nat/>
+            </Columns.Column>
+            <Columns.Column size={6}>
+                <SampleChart2Imm/>
+            </Columns.Column>
+            <Columns.Column size={12}>
+                <SampleChart2/>
+            </Columns.Column>
+        </Columns>
+    </div>
+);
+
+export default App;
