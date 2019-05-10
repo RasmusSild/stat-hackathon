d2e926ac892f4b1524ceb0fd13e43cc9753cc375
RasmusSild <rasmus.sild94@gmail.com>
2019-05-10 09:42:40 +0300

    Updates

---
+++ precache-manifest.2687931918f65d66597fb8ef5921d30a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24615edce3c92804e001dec6917b736b",
+    "url": "/stat-hackathon/index.html"
+  },
+  {
+    "revision": "6e02a5bec7a1b31571dc",
+    "url": "/stat-hackathon/static/css/main.2f7333c8.chunk.css"
+  },
+  {
+    "revision": "32bac9de53f946336b49",
+    "url": "/stat-hackathon/static/js/2.4ece32a3.chunk.js"
+  },
+  {
+    "revision": "6e02a5bec7a1b31571dc",
+    "url": "/stat-hackathon/static/js/main.9b19c8c4.chunk.js"
+  },
+  {
+    "revision": "a3c8c8c034bb7232db4b",
+    "url": "/stat-hackathon/static/js/runtime~main.227ec8d1.js"
+  }
+]);
