ab4307a019bf205f86c577830ad406aa5c2ac3ea
mare hunt <mare.hunt@netgroup.ee>
2019-05-10 13:56:04 +0300

    select

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,6 +3,7 @@ import Container from 'react-bulma-components/lib/components/container';
 import Heading from 'react-bulma-components/lib/components/heading';
 import Hero from 'react-bulma-components/lib/components/hero';
 import Image from 'react-bulma-components/lib/components/image';
+import { Select, Control } from 'react-bulma-components/lib/components/form';
 
 const Landing = () => (
   <>
@@ -16,8 +17,14 @@ const Landing = () => (
   <Hero color="dark">
     <Hero.Body>
         <Container className="has-text-centered">
-        <Heading>
+          <Heading size={2}>
           What interest you most about the future?
+        </Heading>
+          <Heading subtitle size={3} renderAs="h2">
+            <Select>
+              <option>Labour Market</option>
+              <option>Population</option>
+            </Select>
         </Heading>
       </Container>
     </Hero.Body>
